ed77e558087d1f57ae1861194adc87082a8bd9c1
Suchada Khamwan <suchadasaa10@gmail.com>
2022-10-29 22:52:21 +0700

    math

---
+++ math_basic.py
@@ -0,0 +1,9 @@
+# min_score = min(28, 45, 64, 1, 6)
+# max_score = max(28, 45, 64, 1, 6)
+
+score =[28, 45, 64, 1, 6]
+min_score = min(score)
+max_score = max(score)
+
+print(min_score)
+print(max_score)
