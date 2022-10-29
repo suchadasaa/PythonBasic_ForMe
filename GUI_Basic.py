848c2404448e9975f898eefa5a4d28adf5b02d5c
Suchada Khamwan <suchadasaa10@gmail.com>
2022-10-29 19:50:45 +0700

    multi table

--- GUI_Basic.py
+++ GUI_Basic.py
@@ -1,9 +1,14 @@
+from cgitb import text
 import tkinter as tk
 
 window = tk.Tk()
 window.title('JustPython')
 window.minsize(width=400, height=400)
 
+
+def set_message():
+    text = text_input.get()  #ดึงข้อความที่กรอก เอามาใช้งานได้
+    title_level.configure(text=text)  #กำหนดค่าให้เป็นไปตาม text(text_input)
 
 #UI label
 title_level = tk.Label(master=window, text="เมื่อรักฉันเกิด") #master บอกว่าLabelนี้จะถูกใส่ในwindow อะไร
@@ -15,7 +20,7 @@ text_input = tk.Entry(master=window)
 text_input.pack()
 
 #UI button
-ok_button = tk.Button(master=window, text="Okey")
+ok_button = tk.Button(master=window, text="Okey", command=set_message)  #command เมื่อกดแล้วเรียกใช้ฟังก์ชัน **ระวังอย่าเผลอใส่() เพราะฟังก์ชัน อาจจะถูกรันก่อนถูกกด
 ok_button.pack()
 
 
---
+++ multiplication_table.py
@@ -0,0 +1,45 @@
+import tkinter as tk
+from turtle import title
+
+
+def showOutput():
+    number = int(number_input.get())  #เพราะget จะreturn ค่ากลับมาเป็น str จึงต้องแปลงให้เป็นint 
+
+    if number == 0:
+        output_label.configure(text='ผิดที่ไว้ใจ')
+        return
+
+
+    output = ''
+
+    #คำนวณสูตรคูณ
+    for i in range(1,13):
+        output += str(number) + ' x ' + str(i) + ' = '
+        output += str(number * i) + '\n'
+
+
+    #update text in output
+    output_label.configure(text=output)
+
+
+
+window = tk.Tk()
+window.title('JustPython')
+window.minsize(width=400, height=400)
+
+
+title_label = tk.Label(master=window, text="สูตรคูณแม่")
+title_label.pack(pady=20) #pady = pad y เพิ่มพื้นที่ว่างด้านล่าง
+
+number_input = tk.Entry(master=window, width=15)
+number_input.pack(pady=5)
+
+go_button = tk.Button(master=window, text="ได้แก่", command=showOutput, width=10, height=2)
+go_button.pack()
+
+
+output_label = tk.Label(master=window)
+output_label.pack(pady=20)
+
+
+window.mainloop()
